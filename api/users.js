53891ad5668f3bc207d2e8b8cfd76c5dfb91edf0
Lane <Kewllane@aol.com>
2022-08-04 20:28:31 -0500

    Part 2 Part 2 Finished

--- api/users.js
+++ api/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllUsers, getUserByUsername } = require('../db');
+const { getAllUsers, getUserByUsername, createUser } = require('../db');
 const usersRouter = express.Router();
 const jwt = require('jsonwebtoken'); 
 
@@ -10,7 +10,7 @@ usersRouter.use((req, res, next) => {
 });
 
 usersRouter.get('/', async (req, res) => {
-    const users = await getAllUsers();
+  const users = await getAllUsers();
 
   res.send({
     users
@@ -20,7 +20,6 @@ usersRouter.get('/', async (req, res) => {
 usersRouter.post('/login', async (req, res, next) => {
     const { username, password } = req.body;
   
-    // request must have both
     if (!username || !password) {
       next({
         name: "MissingCredentialsError",
@@ -32,8 +31,8 @@ usersRouter.post('/login', async (req, res, next) => {
       const user = await getUserByUsername(username);
   
       if (user && user.password == password) {
-        // create token & return to user
-        res.send({ message: "you're logged in!" });
+        const token = jwt.sign(user, process.env.JWT_SECRET);
+        res.send({ message: "you're logged in!", token });
       } else {
         next({ 
           name: 'IncorrectCredentialsError', 
@@ -44,6 +43,42 @@ usersRouter.post('/login', async (req, res, next) => {
       console.log(error);
       next(error);
     }
+  });
+
+  usersRouter.post('/register', async (req, res, next) => {
+    const { username, password, name, location } = req.body;
+  
+    try {
+      const _user = await getUserByUsername(username);
+  
+      if (_user) {
+        next({
+          name: 'UserExistsError',
+          message: 'A user by that username already exists'
+        });
+      }
+  
+      const user = await createUser({
+        username,
+        password,
+        name,
+        location,
+      });
+  
+      const token = jwt.sign({ 
+        id: user.id, 
+        username
+      }, process.env.JWT_SECRET, {
+        expiresIn: '1w'
+      });
+  
+      res.send({ 
+        message: "thank you for signing up",
+        token 
+      });
+    } catch ({ name, message }) {
+      next({ name, message })
+    } 
   });
 
 module.exports = usersRouter;
